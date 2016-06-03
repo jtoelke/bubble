599c5512188ef3073e6a003904b2c34d28f9a6b3
Jessica Beller <jessibeller@gmail.com>
2016-06-03 12:09:36 -0700

    Split bubble path calculation into functions
    Use find_destination() to calculate intersection with wall
    or ceiling for given line.
    Use shoot to calculate the the line through the tapped position,
    call find_destination and do the animation.

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,5 +1,6 @@
 
 import animate;
+import math.geom.Point as Point;
 import ui.View;
 import ui.ImageView;
 import ui.TextView;
@@ -64,40 +65,49 @@ exports = Class(ui.View, function (supr) {
 		this.on('InputSelect', function (event, point) {
 			console.log("View clicked at position: " + point.x + "," + point.y);
 			if (point.y < bottom) {
-				var dest_x = point.x;
-				var dest_y = point.y;
-
-				var bubble_y = this._current_bubble.style.y + bubble_size/2;
-				var bubble_x = this._current_bubble.style.x + bubble_size/2;
-
-				var slope = (point.y - bubble_y) / (point.x - bubble_x);
-				var con = point.y - slope * point.x;
-				if (point.x < app_width/2) {		// tapped in the left half of the screen
-					console.log("Left wall.");
-					dest_y = slope * left_wall + con;
-					if (dest_y > ceiling) {
-						dest_x = left_wall;
-					}
-				} else if (point.x > app_width/2) {	// tapped in the right half of the screen
-					console.log("Right wall.");
-					dest_y = slope * right_wall + con;
-					if (dest_y > ceiling) {
-						dest_x = right_wall;
-					}
-				}
-				if (dest_x == point.x) {				// hitting ceiling
-					console.log("Ceiling.");
-					dest_y = ceiling;
-					dest_x = (ceiling - con) / slope;
-				}
-
-				dest_x = dest_x - bubble_size/2;
-				dest_y = dest_y - bubble_size/2;
-				console.log("Destination calculated as : " + dest_x + "," + dest_y);
-				var animator = animate(this._current_bubble);
-				animate(this._current_bubble).now({x: dest_x, y: dest_y}, 500).then({x: current_bubble_x, y: current_bubble_y}, 500);
+				this.shoot(point);
 			}
 		});
+
+		this.shoot = function (point) {
+			var bubble_y = this._current_bubble.style.y + bubble_size/2;
+			var bubble_x = this._current_bubble.style.x + bubble_size/2;
+			var slope = (point.y - bubble_y) / (point.x - bubble_x);
+			var con = point.y - slope * point.x;
+
+			var dest = this.find_destination(point, slope, con);
+
+			dest.x = dest.x - bubble_size/2;
+			dest.y = dest.y - bubble_size/2;
+			console.log("Destination calculated as : " + dest.x + "," + dest.y);
+			var animator = animate(this._current_bubble);
+			animate(this._current_bubble).now({x: dest.x, y: dest.y}, 500);
+			animate(this._current_bubble).then({x: current_bubble_x, y: current_bubble_y}, 500);
+		}
+
+		this.find_destination = function (start_pos, slope, con) {
+			var dest_x = start_pos.x;
+			var dest_y = start_pos.y;
+			if (start_pos.x < app_width/2) {		// tapped in the left half of the screen
+				console.log("Left wall.");
+				dest_y = slope * left_wall + con;
+				if (dest_y > ceiling) {
+					dest_x = left_wall;
+				}
+			} else if (start_pos.x > app_width/2) {	// tapped in the right half of the screen
+				console.log("Right wall.");
+				dest_y = slope * right_wall + con;
+				if (dest_y > ceiling) {
+					dest_x = right_wall;
+				}
+			}
+			if (dest_x == start_pos.x) {			// hitting ceiling
+				console.log("Ceiling.");
+				dest_y = ceiling;
+				dest_x = (ceiling - con) / slope;
+			}
+			return dest = new Point({x: dest_x, y: dest_y});
+		}
 
 		this._ceiling = new ui.ImageView({
 			superview: this,
@@ -123,7 +133,7 @@ exports = Class(ui.View, function (supr) {
 			width: cannon_top_img.getWidth(),
 			height: cannon_top_img.getHeight()
 		});
-
+/*
 		this._bubbles = [];
 
 		for (var row = 0; row < row_amount; row++) {
@@ -139,7 +149,7 @@ exports = Class(ui.View, function (supr) {
 				this.addSubview(bubble);
 				this._bubbles.push(bubble);
 			}
-		}
+		}*/
 
 		this._current_bubble = new ui.ImageView({
 								superview: this,
